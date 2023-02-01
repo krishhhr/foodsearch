7969d576aa43199096ed9bdd55080f0b866b0d77
Krish <krishranawat301@gmail.com>
2023-02-01 11:20:34 +0530

    sns

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react';
 import './App.css';
+import Card from '@mui/material/Card';
+
 
 function App() {
+
+  const [text,setText]=useState('')
+  const [container,setContainer]=useState([])
+  const [final,setFinal]=useState('')
+
+  useEffect(()=>{
+    // console.log("Hi");
+      const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'a559ec0f00mshdca23a1ff8f2c7ep178543jsn751f64ee02ad',
+        'X-RapidAPI-Host': 'edamam-food-and-grocery-database.p.rapidapi.com'
+      }
+    };
+    
+    fetch(`https://edamam-food-and-grocery-database.p.rapidapi.com/parser?ingr=+${text}`, options)
+      .then(response => {
+        return response.json()
+      })
+      .then(data=>{
+        setContainer(data.hints)
+        // console.log(data);
+      })
+      .catch(err => console.error(err));
+  },[final])
+
+
+
+    const submitHandler=e=>{
+      e.preventDefault();
+      setFinal(text)
+      // console.log(final);
+    }
+
+  
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <form onSubmit={submitHandler}>
+      <input type='text' value={text} onChange={(e)=>setText(e.target.value)} placeholder='Search here'/>
+      <button type='submit'>Submit</button>
+<div>
+      {
+        container?.map((obj,index)=>{
+          return(
+            <Card key={index}  
+            
+            sx={{width:'200px',float:'left',minHeight:'400px',m:'5px'}}>
+              <img src={obj.food.image} alt={obj.food.label}/>
+              <p>{obj.food.label}</p>
+              <span id='box'>{obj.food.category}</span>
+            </Card>
+          )
+        })
+      }
+      </div>
+      </form>
+  )
+    }
+
 
 export default App;
